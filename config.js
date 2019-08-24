39a6457d0c4337db4ac4758c08e220a84e202722
Adam Gall <adam@decentlabs.io>
2019-08-24 10:57:18 -0400

    Update configuration script to handle taking arrays of vesting durations

--- config.js
+++ config.js
@@ -2,44 +2,51 @@ require('dotenv').config()
 
 const SECONDS_PER_MONTH = 2628000;
 
-const MONTHS_TO_CLIFF = process.env.MONTHS_TO_CLIFF;
-const MONTHS_TO_RELEASE = process.env.MONTHS_TO_RELEASE;
+const MONTHS_TO_CLIFF = process.env.MONTHS_TO_CLIFF.split(',').map(i => i.trim());
+const MONTHS_TO_RELEASE = process.env.MONTHS_TO_RELEASE.split(',').map(i => i.trim());
 const TOKEN_ADDRESS = process.env.TOKEN_ADDRESS
-const VESTED_TOKENS = process.env.VESTED_TOKENS.split(',');
-const BENEFICIARY_ADDRESSES = process.env.BENEFICIARY_ADDRESSES.split(',');
+const VESTED_TOKENS = process.env.VESTED_TOKENS.split(',').map(i => i.trim());
+const BENEFICIARY_ADDRESSES = process.env.BENEFICIARY_ADDRESSES.split(',').map(i => i.trim());
 const REVOKABLE = process.env.REVOKABLE === 'true';
 
-if (BENEFICIARY_ADDRESSES.length != VESTED_TOKENS.length) {
-  throw new ReferenceError(
-    `Addresses length (${BENEFICIARY_ADDRESSES.length}) must equal vested tokens length (${VESTED_TOKENS.length})`
-  )
+if (
+  BENEFICIARY_ADDRESSES.length != VESTED_TOKENS.length ||
+  VESTED_TOKENS.length != MONTHS_TO_CLIFF.length ||
+  MONTHS_TO_CLIFF.length != MONTHS_TO_RELEASE.length
+) {
+  throw new ReferenceError("Lengths of inputs don't match up")
 }
 
-VESTED_TOKENS.forEach(amount => {
-  if (amount % MONTHS_TO_RELEASE !== 0) {
+for (let i = 0; i < VESTED_TOKENS.length; i++) {
+  const amount = VESTED_TOKENS[i];
+  const monthsToRelease = MONTHS_TO_RELEASE[i];
+
+  if (amount % monthsToRelease !== 0) {
     throw new ReferenceError(
-      `Total vested token amount (${amount}) is not cleanly divisible by the months (${MONTHS_TO_RELEASE})`
+      `Total vested token amount (${amount}) is not cleanly divisible by the months (${monthsToRelease})`
     );
   }
-});
-
-const TOKENS_PER_MONTH = VESTED_TOKENS.map(amount => amount / MONTHS_TO_RELEASE);
-
-const CLIFF_DURATION = SECONDS_PER_MONTH * MONTHS_TO_CLIFF;
-const RELEASABLE_DURATION = SECONDS_PER_MONTH * MONTHS_TO_RELEASE;
-const TOTAL_VEST_DURATION = RELEASABLE_DURATION + CLIFF_DURATION;
-
-if (RELEASABLE_DURATION % SECONDS_PER_MONTH !== 0) {
-  throw new ReferenceError("Releasable duration is not a multiple of months");
 }
+
+const TOKENS_PER_MONTH = VESTED_TOKENS.map((amount, i) => amount / MONTHS_TO_RELEASE[i]);
+
+const CLIFF_DURATIONS = MONTHS_TO_CLIFF.map(months => months * SECONDS_PER_MONTH);
+const RELEASABLE_DURATIONS = MONTHS_TO_RELEASE.map(months => months * SECONDS_PER_MONTH);
+const TOTAL_VEST_DURATIONS = RELEASABLE_DURATIONS.map((dur, i) => dur + CLIFF_DURATIONS[i]);
+
+RELEASABLE_DURATIONS.forEach(d => {
+  if (d % SECONDS_PER_MONTH !== 0) {
+    throw new ReferenceError(`Releasable duration ${d} is not a multiple of months`);
+  }
+});
 
 module.exports = {
   SECONDS_PER_MONTH,
   MONTHS_TO_RELEASE,
   VESTED_TOKENS,
   TOKENS_PER_MONTH,
-  CLIFF_DURATION,
-  TOTAL_VEST_DURATION,
+  CLIFF_DURATIONS,
+  TOTAL_VEST_DURATIONS,
   BENEFICIARY_ADDRESSES,
   TOKEN_ADDRESS,
   REVOKABLE
